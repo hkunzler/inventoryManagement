1811cca2e6d94e576980f271b79b4bb13df8df5d
Harmony Kunzler <hkunzler@gmail.com>
2022-07-24 09:34:40 -0600

    parts added to products screen

--- src/main/java/com/inventorymanagementsystem/hkunzler_software1_pa/ProductFormController.java
+++ src/main/java/com/inventorymanagementsystem/hkunzler_software1_pa/ProductFormController.java
@@ -3,14 +3,13 @@ package com.inventorymanagementsystem.hkunzler_software1_pa;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class ProductFormController {
+public class ProductFormController  {
     @FXML
     public Label productFormTitle;
 
-    public void setProductFormTitle(String data) {
-        productFormTitle.setText(data);
-    }
     public void setHeader(String partFormTitle) {
-        this.productFormTitle.setText(partFormTitle + " Part");
+        this.productFormTitle.setText(partFormTitle + " Product");
     }
+    @FXML
+    TableViewController partsTableController;
 }
--- src/main/java/com/inventorymanagementsystem/hkunzler_software1_pa/TableViewController.java
+++ src/main/java/com/inventorymanagementsystem/hkunzler_software1_pa/TableViewController.java
@@ -1,5 +1,32 @@
 package com.inventorymanagementsystem.hkunzler_software1_pa;
 
-public class TableViewController {
+import com.inventorymanagementsystem.hkunzler_software1_pa.models.Part;
+import com.inventorymanagementsystem.hkunzler_software1_pa.models.PartInventory;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TableViewController implements Initializable {
+    @FXML
+    private TableView<Part> partTable;
+    @FXML
+    private TableColumn<Part, Integer> itemStock;
+    @FXML
+    private TableColumn<Part, Integer> itemPrice;
+    @FXML
+    private TableColumn<Part, Integer> itemID;
+    @FXML
+    private TableColumn<Part, String> itemName;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+                partTable.setItems(PartInventory.getParts());
+        Platform.runLater(() -> {
+        });
+    }
 }
